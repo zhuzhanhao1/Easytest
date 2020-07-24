45c61b7762b096b4e7398b37fff8951a12f5a01a
zhuzhanhao <971567069@qq.com>
2020-07-24 19:55:18 +0800

    添加拉取镜像脚本、推送镜像脚本

--- pullRegistry.sh
+++ pullRegistry.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #首次登入阿里云镜像仓库成功后会有一个警告，意思你的密码以明文格式记录在/root/.docker/config.json
-RegistryAddr="registry.cn-hangzhou.aliyuncs.com/zhuzhanhao/zhuzh"
+
+#阿里云镜像库
+#RegistryAddr="registry.cn-hangzhou.aliyuncs.com/zhuzhanhao/zhuzh"
+#公司私服库地址
+RegistryAddr="mvn.docworks.cn:9082/zhuzh"
+
 #登录阿里云镜像库，这里已经登录过阿里云镜像库了，则不需要登录了
 #docker login --username=${Username} ${Registry_Domain}  -p${Password}
 #停止容器并删除容器
@@ -13,6 +18,3 @@ docker run -d -p 8000:8000 \
       --restart=always \
       -v /amberdata/Easytest:/usr/src/app \
       ${RegistryAddr}:1.0
-
-
-
--- pushRegistry.sh
+++ pushRegistry.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+#阿里云镜像库
+#RegistryAddr="registry.cn-hangzhou.aliyuncs.com/zhuzhanhao/zhuzh"
+#公司私服库地址
+RegistryAddr="mvn.docworks.cn:9082/zhuzh"
+#docker tag zhuzh_web:latest ${RegistryAddr}:1.0
+
 #共有网络，将镜像推送到Registry
-docker tag zhuzh_web:latest registry.cn-hangzhou.aliyuncs.com/zhuzhanhao/zhuzh:1.0
-docker push registry.cn-hangzhou.aliyuncs.com/zhuzhanhao/zhuzh:1.0
+docker push ${RegistryAddr}:1.0
 
 #VPC网络，将镜像推送到Registry
 #docker tag jenkinsci/blueocean:latest registry-vpc.cn-hangzhou.aliyuncs.com/zhuzhanhao/zhuzh:1.0
